7ff50156733d868e1c6b7e5c84c30ae615f08713
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2022-07-19 00:41:48 +0300

    12

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import validatePasword from "./js/validate-password";
+import { addUser } from "./js/api-service";
+
+addUser("mango");
+// console.log(validatePasword("qweqweffgghdffghdfgfh"));
+// import {
+//   fetchAllUsers,
+//   fetchUserById,
+//   updateUserById,
+//   x as value,
+//   y as name,
+// } from "./js/api-service";
+
+// import * as apiService from "./js/api-service";
+
+// console.log(validatePasword);
+
+// console.log(validatePasword("qwerqweqwwqe"));
+
+// console.log(fetchAllUsers);
+// console.log(fetchUserById);
+// console.log(updateUserById);
+// console.log(value);
+
+// console.log(apiService);
---
+++ src/js/api-service.js
@@ -0,0 +1,25 @@
+import shortid from "shortid";
+
+export const fetchAllUsers = () => {
+  console.log("fetchAllUsers");
+};
+
+export const fetchUserById = (id) => {
+  console.log(fetchUserById);
+};
+
+export const updateUserById = (id) => {
+  console.log(updateUserById);
+};
+
+export const x = 5;
+
+export const y = "qwerqwweqwe";
+
+export const addUser = (name) => {
+  const user = {
+    id: shortid.generate(),
+    name,
+  };
+  console.log(user);
+};
